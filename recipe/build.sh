2f0a23624cfda97871b380ad65911ca734f1c8b0
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-03-21 15:44:11 -0400

    recipe: Add C++ support to protobuf. [skip appveyor]

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-# Change the permissions of the protobuf.egg-info directory to be 
-# group and world readable
-chmod 644 protobuf.egg-info/*
-$PYTHON setup.py install --single-version-externally-managed --record record.txt
+
+chmod +x ./configure
+
+# Rebuild configure/Makefile.am as they are broken.
+aclocal
+automake
+autoconf
+
+# For gtest rebuild configure/Makefile.am as they are broken.
+pushd ./gtest
+aclocal
+automake
+autoconf
+popd
+
+./configure --prefix=$PREFIX
+
+make
+make check
+make install
+(cd python && python setup.py install --single-version-externally-managed --record record.txt && cd ..)
