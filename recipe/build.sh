1e9c37d1aaf6e5bc2536190d558d3bea35a6b2a8
Jonathan Helmus <jjhelmus@gmail.com>
2018-08-14 17:04:04 -0500

    remove __init__.py fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,13 +11,6 @@ fi
 # Install python package now
 cd python
 
-# Begin fix for missing packages issue: https://github.com/conda-forge/protobuf-feedstock/issues/40
-mkdir -p google/protobuf/util
-mkdir -p google/protobuf/compiler
-touch google/protobuf/util/__init__.py
-touch google/protobuf/compiler/__init__.py
-# End fix
-
 if [[ ${PY_VER} == 3.7 ]]; then
   # https://github.com/google/protobuf/issues/4086
   export CFLAGS="${CFLAGS} -fpermissive"
