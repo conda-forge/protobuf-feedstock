b23d7f01e22974821fa079e21b3acf76dc236abf
Julien Schueller <schueller@phimeca.com>
2020-08-04 08:54:57 +0200

    Drop -fpermissive flag

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,9 +15,4 @@ fi
 # Install python package now
 cd python
 
-if [[ ${PY_VER} == 3.7 ]]; then
-  # https://github.com/google/protobuf/issues/4086
-  export CFLAGS="${CFLAGS} -fpermissive"
-fi
-
 ${PYTHON} setup.py install --cpp_implementation --single-version-externally-managed --record record.txt
