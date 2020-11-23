50bc66fc53da8f8be65bec24650454087efb21c4
Isuru Fernando <isuruf@gmail.com>
2020-11-22 23:47:22 -0600

    Cleanup build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-
-if [ "$(uname)" == "Darwin" ];
-then
-    # Switch to clang with C++11 ASAP.
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-    export LIBS="-lc++"
-fi
-
-if [ `uname` == Linux ]; then
-    export CFLAGS="${CFLAGS} --std=c++11"
-fi
-
 # Install python package now
 cd python
 
