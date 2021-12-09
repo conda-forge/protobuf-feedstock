d605468d6e69c51d6b77103856c9559f771ca580
Carlo <carlodri@users.noreply.github.com>
2021-12-09 11:28:51 +0100

    try install command

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,8 +4,5 @@ if errorlevel 1 exit 1
 cd python
 if errorlevel 1 exit 1
 
-"%PYTHON%" setup.py build --cpp_implementation
-if errorlevel 1 exit 1
-"%PYTHON%" setup.py test --cpp_implementation
-if errorlevel 1 exit 1
 "%PYTHON%" setup.py install --cpp_implementation
+if errorlevel 1 exit 1
