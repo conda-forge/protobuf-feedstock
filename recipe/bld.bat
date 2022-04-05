ef932a827b1ed19daa445590b1823ed281529e32
John Kirkham <jakirkham@gmail.com>
2022-04-04 20:13:12 -0700

    Use `script` to perform install

--- recipe/bld.bat
+++
@@ -1,8 +0,0 @@
-:: Install the Python portions too.
-cd %SRC_DIR%
-if errorlevel 1 exit 1
-cd python
-if errorlevel 1 exit 1
-
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
-if errorlevel 1 exit 1
