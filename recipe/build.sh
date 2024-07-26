e1e6444ff5d856727a21b4eb3977c62e9b74925b
H. Vetinari <h.vetinari@gmx.com>
2024-07-26 21:06:15 +1100

    explicitly use standalone bazel

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,6 +16,6 @@ ls -R ../bazel-standalone
 
 export BAZEL="$(pwd)/../bazel-standalone"
 export BAZEL_BUILD_OPTS="--platforms=//bazel_toolchain:target_platform --host_platform=//bazel_toolchain:build_platform --extra_toolchains=//bazel_toolchain:cc_cf_toolchain --extra_toolchains=//bazel_toolchain:cc_cf_host_toolchain"
-../bazel build //python/dist:binary_wheel --define=use_fast_cpp_protos=true
+../bazel-standalone build //python/dist:binary_wheel --define=use_fast_cpp_protos=true
 
 python -m pip install -f ./bazel-bin/python/dist protobuf==$PKG_VERSION
