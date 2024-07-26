e666256b18cc91b8a93e9a3f4f4645599ec75109
H. Vetinari <h.vetinari@gmx.com>
2024-07-26 17:00:53 +1100

    debug...

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,6 +11,9 @@ if [[ "$build_platform" == "$target_platform" ]]; then
     export PROTOC=$BUILD_PREFIX/bin/protoc
 fi
 
+ls -R ../bazel
+ls -R ../bazel-standalone
+
 export BAZEL="$(pwd)/../bazel-standalone"
 export BAZEL_BUILD_OPTS="--platforms=//bazel_toolchain:target_platform --host_platform=//bazel_toolchain:build_platform --extra_toolchains=//bazel_toolchain:cc_cf_toolchain --extra_toolchains=//bazel_toolchain:cc_cf_host_toolchain"
 ../bazel build //python/dist:binary_wheel --define=use_fast_cpp_protos=true
