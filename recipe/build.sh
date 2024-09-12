7394445d0244993e07f2266f2f9e6db0e8fcd72d
Uwe L. Korn <uwe.korn@quantco.com>
2024-09-12 20:57:42 +0200

    Bazel build fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,7 +16,15 @@ ls -R ../bazel
 ls -R ../bazel-standalone
 
 export BAZEL="$(pwd)/../bazel-standalone"
-export BAZEL_BUILD_OPTS="--platforms=//bazel_toolchain:target_platform --host_platform=//bazel_toolchain:build_platform --extra_toolchains=//bazel_toolchain:cc_cf_toolchain --extra_toolchains=//bazel_toolchain:cc_cf_host_toolchain"
-../bazel-standalone build //python/dist:binary_wheel --define=use_fast_cpp_protos=true
+../bazel-standalone build \
+    --platforms=//bazel_toolchain:target_platform \
+    --host_platform=//bazel_toolchain:build_platform \
+    --extra_toolchains=//bazel_toolchain:cc_cf_toolchain \
+    --extra_toolchains=//bazel_toolchain:cc_cf_host_toolchain \
+    --crosstool_top=//bazel_toolchain:toolchain \
+    --cpu=${TARGET_CPU} \
+    --local_cpu_resources=${CPU_COUNT} \
+    //python/dist:binary_wheel \
+    --define=use_fast_cpp_protos=true
 
-python -m pip install -f ./bazel-bin/python/dist protobuf==$PKG_VERSION
+python -m pip install ../bazel-bin/python/dist/protobuf-${PKG_VERSION}-*.whl
