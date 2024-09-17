38acedd0c85d3e729ebe0d98ba2f93db3fd125e0
Uwe L. Korn <uwe.korn@quantco.com>
2024-09-17 20:59:09 +0200

    Only shorten output base in CI

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -20,7 +20,15 @@ cd python
 
 set PROTOC=%LIBRARY_BIN%\protoc
 
-..\bazel --output_base="%CONDA_BLD_PATH%bazel" build --subcommands --action_env PYTHON_BIN_PATH=%PYTHON% --extra_toolchains=//py_toolchain:py_toolchain //python/dist:binary_wheel --define=use_fast_cpp_protos=true
+@rem Shorten path in CI
+@rem See https://github.com/bazelbuild/bazel/issues/18683 and https://github.com/protocolbuffers/protobuf/issues/12947
+if defined CONDA_BLD_PATH (
+  set OUTPUT_BASE="--output_base=%CONDA_BLD_PATH%bazel"
+) else (
+  set OUTPUT_BASE=
+)
+
+..\bazel %CONDA_BLD_PATH% build --subcommands --action_env PYTHON_BIN_PATH=%PYTHON% --extra_toolchains=//py_toolchain:py_toolchain //python/dist:binary_wheel --define=use_fast_cpp_protos=true
 if %ERRORLEVEL% neq 0 exit 1
 
 :: `pip install dist\protobuf*.whl` does not work on windows,
