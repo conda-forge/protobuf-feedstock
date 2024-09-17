2454f03f2bec23e38a743b3898026098698de9b5
Uwe L. Korn <uwe.korn@quantco.com>
2024-09-17 15:53:55 +0200

    output_base must be before build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -14,17 +14,13 @@ for /f "delims=" %%i in (%RECIPE_DIR%\py_toolchain_win.bzl) do (
 )
 
 sed -i "s/ SYSTEM_PYTHON_VERSION/ %PY_VER_NO_DOT%/g" python\dist\dist.bzl
-
-type python\dist\dist.bzl
+if %ERRORLEVEL% neq 0 exit 1
 
 cd python
 
 set PROTOC=%LIBRARY_BIN%\protoc
 
-..\bazel query "//python/dist:*"
-if %ERRORLEVEL% neq 0 exit 1
-
-..\bazel build --output_base="D:\\bld\\bazel" --subcommands --action_env PYTHON_BIN_PATH=%PYTHON% --extra_toolchains=//py_toolchain:py_toolchain //python/dist:binary_wheel --define=use_fast_cpp_protos=true
+..\bazel --output_base="%CONDA_BLD_PATH%bazel" build --subcommands --action_env PYTHON_BIN_PATH=%PYTHON% --extra_toolchains=//py_toolchain:py_toolchain //python/dist:binary_wheel --define=use_fast_cpp_protos=true
 if %ERRORLEVEL% neq 0 exit 1
 
 :: `pip install dist\protobuf*.whl` does not work on windows,
