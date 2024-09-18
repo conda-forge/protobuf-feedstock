81622d8dd46990c757f20af63d27eddf81a71f3e
Uwe L. Korn <uwe.korn@quantco.com>
2024-09-18 09:31:48 +0200

    Add py_cc toolchain

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,15 +3,47 @@ setlocal enabledelayedexpansion
 
 md py_toolchain
 
+xcopy /s /y /i %PREFIX%\libs %SRC_DIR%\py_toolchain\libs
+if %ERRORLEVEL% neq 0 exit 1
+
+xcopy /s /y /i %PREFIX%\include %SRC_DIR%\py_toolchain\include
+if %ERRORLEVEL% neq 0 exit 1
+
 set "PYTHON_CYGPATH=%PYTHON:\=/%"
 set "PY_VER_NO_DOT=%PY_VER:.=%"
+
+for /f "tokens=1,2 delims=." %%a in ("%PY_VER%") do (
+    set PY_VER_MAJOR=%%a
+    set PY_VER_MINOR=%%b
+)
+
 set
 
-for /f "delims=" %%i in (%RECIPE_DIR%\py_toolchain_win.bzl) do (
-    set line=%%i
-    set "line=!line:PYTHON_EXE=%PYTHON_CYGPATH%!"
-    echo !line! >> %SRC_DIR%\py_toolchain\BUILD
-)
+@rem for /f "delims=" %%i in (%RECIPE_DIR%\py_toolchain_win.bzl) do (
+@rem     set line=%%i
+@rem     set "line=!line:PYTHON_EXE=%PYTHON_CYGPATH%!"
+@rem     echo !line! >> %SRC_DIR%\py_toolchain\BUILD
+@rem )
+
+copy %RECIPE_DIR%\py_toolchain_win.bzl %SRC_DIR%\py_toolchain\BUILD
+if %ERRORLEVEL% neq 0 exit 1
+
+sed -i "s;PYTHON_EXE;%PYTHON_CYGPATH%;g" %SRC_DIR%\py_toolchain\BUILD
+if %ERRORLEVEL% neq 0 exit 1
+
+sed -i "s;PY_VER_NO_DOT;%PY_VER_NO_DOT%;g" %SRC_DIR%\py_toolchain\BUILD
+if %ERRORLEVEL% neq 0 exit 1
+
+sed -i "s;PY_VER;%PY_VER%;g" %SRC_DIR%\py_toolchain\BUILD
+if %ERRORLEVEL% neq 0 exit 1
+
+sed -i "s;PY_VER_MAJOR;%PY_VER_MAJOR%;g" %SRC_DIR%\py_toolchain\BUILD
+if %ERRORLEVEL% neq 0 exit 1
+
+sed -i "s;PY_VER_MINOR;%PY_VER_MINOR%;g" %SRC_DIR%\py_toolchain\BUILD
+if %ERRORLEVEL% neq 0 exit 1
+
+type %SRC_DIR%\py_toolchain\BUILD
 
 sed -i "s/ SYSTEM_PYTHON_VERSION/ %PY_VER_NO_DOT%/g" python\dist\dist.bzl
 if %ERRORLEVEL% neq 0 exit 1
@@ -28,7 +60,7 @@ if defined CONDA_BLD_PATH (
   set OUTPUT_BASE=
 )
 
-..\bazel %OUTPUT_BASE% build --subcommands --action_env PYTHON_BIN_PATH=%PYTHON% --extra_toolchains=//py_toolchain:py_toolchain //python/dist:binary_wheel --define=use_fast_cpp_protos=true
+..\bazel %OUTPUT_BASE% build --subcommands --action_env PYTHON_BIN_PATH=%PYTHON% --extra_toolchains=//py_toolchain:py_cc --extra_toolchains=//py_toolchain:py_toolchain //python/dist:binary_wheel --define=use_fast_cpp_protos=true
 if %ERRORLEVEL% neq 0 exit 1
 
 :: `pip install dist\protobuf*.whl` does not work on windows,
--- recipe/py_toolchain_win.bzl
+++ recipe/py_toolchain_win.bzl
@@ -1,9 +1,14 @@
 load("@bazel_tools//tools/python:toolchain.bzl", "py_runtime_pair")
+load("@rules_python//python/cc:py_cc_toolchain.bzl", "py_cc_toolchain")
 
 py_runtime(
     name = "python3",
     python_version = "PY3",
     interpreter_path = "PYTHON_EXE",
+    interpreter_version_info = {{
+        "major": "PY_VER_MAJOR",
+        "minor": "PY_VER_MINOR",
+    }},
 )
 
 py_runtime_pair(
@@ -17,3 +22,38 @@ toolchain(
     toolchain_type = "@bazel_tools//tools/python:toolchain_type",
 )
 
+toolchain(
+    name = "py_cc",
+    toolchain = ":py_cc_toolchain",
+    toolchain_type = "@rules_python//python/cc:toolchain_type",
+)
+
+py_cc_toolchain(
+    name = "py_cc_toolchain",
+    headers = ":headers",
+    libs = ":libraries",
+    python_version = "PY_VER",
+)
+
+cc_library(
+    name = "headers",
+    hdrs = glob(["include/**/*.h"]),
+    includes = ["include"],
+    deps = select({{
+        "@platforms//os:windows": [":interface_library"],
+        "//conditions:default": [],
+    }}),
+)
+
+cc_import(
+    name = "interface_library",
+    interface_library = select({{
+        "@platforms//os:windows": "libs/pythonPY_VER_NO_DOT.lib",
+        "//conditions:default": None,
+    }}),
+    system_provided = True,
+)
+
+cc_library(
+    name = "libraries",
+)
