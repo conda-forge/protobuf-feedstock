a66baf3bc46d1e3447ff4e0cf3086d7552019399
Uwe L. Korn <uwe.korn@quantco.com>
2024-09-18 21:26:10 +0200

    Explicit pip install

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -60,6 +60,5 @@ if defined CONDA_BLD_PATH (
 ..\bazel %OUTPUT_BASE% build --subcommands --linkopt "/LIBPATH:%PREFIX%\libs" --action_env "LIB=/LIBPATH:%PREFIX%\libs" --action_env PYTHON_BIN_PATH=%PYTHON% --extra_toolchains=//py_toolchain:py_cc --extra_toolchains=//py_toolchain:py_toolchain //python/dist:binary_wheel --define=use_fast_cpp_protos=true
 if %ERRORLEVEL% neq 0 exit 1
 
-for %%f in (bazel-bin\python\dist\protobuf-*.whl) do (
-  python -m pip install %%f
-)
+python -m pip install ..\bazel-bin\python\dist\protobuf-%PKG_VERSION%-cp%PY_VER_NO_DOT%-abi3-win_amd64.whl
+if %ERRORLEVEL% neq 0 exit 1
