3351f3b62f5f3ec0ff72b4c93a8d04f8a466b9e0
Uwe L. Korn <uwe.korn@quantco.com>
2024-09-19 20:13:43 +0200

    Fix patches

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,7 +9,7 @@ set "PY_VER_NO_DOT=%PY_VER:.=%"
 sed -i "s/ SYSTEM_PYTHON_VERSION/ %PY_VER_NO_DOT%/g" python\dist\dist.bzl
 if %ERRORLEVEL% neq 0 exit 1
 
-sed -i "s;PYTHON_EXE;%PYTHON_CYGPATH%;g" %SRC_DIR%\bazel\system_python.bzl
+sed -i "s;PYTHON_EXE;%PYTHON_CYGPATH%;g" %SRC_DIR%\python\dist\system_python.bzl
 if %ERRORLEVEL% neq 0 exit 1
 
 cd python
