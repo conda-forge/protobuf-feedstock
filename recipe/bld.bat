58b9abf4eed3c587227014fe989f24bde1867d57
Uwe L. Korn <uwe.korn@quantco.com>
2024-09-19 11:55:40 +0200

    Drop py_toolchain on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,33 +6,6 @@ md py_toolchain
 set "PYTHON_CYGPATH=%PYTHON:\=/%"
 set "PY_VER_NO_DOT=%PY_VER:.=%"
 
-for /f "tokens=1,2 delims=." %%a in ("%PY_VER%") do (
-    set PY_VER_MAJOR=%%a
-    set PY_VER_MINOR=%%b
-)
-
-set
-
-copy %RECIPE_DIR%\py_toolchain_win.bzl %SRC_DIR%\py_toolchain\BUILD
-if %ERRORLEVEL% neq 0 exit 1
-
-sed -i "s;PYTHON_EXE;%PYTHON_CYGPATH%;g" %SRC_DIR%\py_toolchain\BUILD
-if %ERRORLEVEL% neq 0 exit 1
-
-sed -i "s;PY_VER_NO_DOT;%PY_VER_NO_DOT%;g" %SRC_DIR%\py_toolchain\BUILD
-if %ERRORLEVEL% neq 0 exit 1
-
-sed -i "s;PY_VER_MAJOR;%PY_VER_MAJOR%;g" %SRC_DIR%\py_toolchain\BUILD
-if %ERRORLEVEL% neq 0 exit 1
-
-sed -i "s;PY_VER_MINOR;%PY_VER_MINOR%;g" %SRC_DIR%\py_toolchain\BUILD
-if %ERRORLEVEL% neq 0 exit 1
-
-sed -i "s;PY_VER;%PY_VER%;g" %SRC_DIR%\py_toolchain\BUILD
-if %ERRORLEVEL% neq 0 exit 1
-
-type %SRC_DIR%\py_toolchain\BUILD
-
 sed -i "s/ SYSTEM_PYTHON_VERSION/ %PY_VER_NO_DOT%/g" python\dist\dist.bzl
 if %ERRORLEVEL% neq 0 exit 1
 
@@ -51,7 +24,7 @@ if defined CONDA_BLD_PATH (
   set OUTPUT_BASE=
 )
 
-..\bazel %OUTPUT_BASE% build --linkopt "/LIBPATH:%PREFIX%\libs" --action_env PYTHON_BIN_PATH=%PYTHON% --extra_toolchains=//py_toolchain:py_toolchain //python/dist:binary_wheel --define=use_fast_cpp_protos=true
+..\bazel %OUTPUT_BASE% build --linkopt "/LIBPATH:%PREFIX%\libs" --action_env PYTHON_BIN_PATH=%PYTHON% //python/dist:binary_wheel --define=use_fast_cpp_protos=true
 if %ERRORLEVEL% neq 0 exit 1
 
 %PYTHON% -m pip install ..\bazel-bin\python\dist\protobuf-%PKG_VERSION%-cp%PY_VER_NO_DOT%-abi3-win_amd64.whl
